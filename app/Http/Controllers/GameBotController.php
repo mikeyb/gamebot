bf024cde9cfcc33ce0b59ca1f938ad15aca1c3d3
mikeyb <mike@mikes.email>
2017-06-24 02:27:14 -0700

    :sweat:

--- app/Http/Controllers/GameBotController.php
+++ app/Http/Controllers/GameBotController.php
@@ -26,7 +26,7 @@ class GameBotController extends Controller
         $botman->verifyServices(env('TOKEN_VERIFY'));
 
         $botman->hears('!prices',
-            function($bot)
+            function(BotMan $bot)
             {
 
                 $prices = $this->events->getDefaultPrices();
@@ -38,15 +38,15 @@ class GameBotController extends Controller
 *GAME* | `https://coinmarketcap.com/currencies/gamecredits/`
 
 ```
-  USD: ' . $prices['game'][0]->price_usd . '
-  BTC: ' . $prices['game'][0]->price_btc . '
+USD: ' . $prices['game'][0]->price_usd . '
+BTC: ' . $prices['game'][0]->price_btc . '
 ```
 
 *MGO* | `https://coinmarketcap.com/currencies/mobilego/`
 
 ```
-  USD: ' . $prices['mog'][0]->price_usd . '
-  BTC: ' . $prices['mog'][0]->price_btc . '
+USD: ' . $prices['mog'][0]->price_usd . '
+BTC: ' . $prices['mog'][0]->price_btc . '
 ```
                 ';
 
@@ -55,6 +55,30 @@ class GameBotController extends Controller
             }
 
         );
+
+        $botman->hears('!price {token}',
+            function (BotMan $bot, $token)
+            {
+
+                $user = $bot->getUser();
+
+                $prices = $this->events->getToken($token);
+
+                $message = '|-> Brought to you by `https://GameBot.chat` <-|
+
+*' . $prices[0]->symbol . '* | `https://coinmarketcap.com/currencies/' . $prices[0]->id . '/`
+
+```
+USD: ' . $prices[0]->price_usd . '
+BTC: ' . $prices[0]->price_btc . '
+```
+                ';
+
+                $bot->reply($message);       
+
+            }
+
+        );        
 
         $botman->listen();
     }
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::match(['get', 'post'], '/slack/gamebot', 'GameBotController@handle');
+
+Route::get('/', function () {
+	return 'Coming Soon';
+});
